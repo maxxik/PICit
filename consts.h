5d5f43d13e48d2bfd5cbf9a5fc6a6c7d75b3ed18
maxxik <maxikka@gmail.com>
2021-10-27 10:05:29 +0200

    init

---
+++ consts.h
@@ -0,0 +1,83 @@
+//  consts.h
+//  PICit!
+//
+
+
+constexpr double     PI             = 3.141592653589793;          // mathematical constant Pi
+constexpr double     TWO_PI         = 2.0 * PI;                   // two times Pi
+constexpr double     SQRT3          = 1.73205080756887729;        // sqrt(3)
+constexpr double     SQRT2          = 1.41421356237309504;        // sqrt(2)
+constexpr double     K_BOLTZMANN    = 1.38064852e-23;             // Boltzmann's constant [J/K]
+constexpr double     EPSILON0       = 8.85418781e-12;             // permittivity of free space [F/m]
+constexpr double     E_CHARGE       = 1.60217662e-19;             // electron charge [C]
+constexpr double     E_MASS         = 9.10938356e-31;             // mass of electron [kg]
+constexpr double     EV_TO_J        = E_CHARGE;                   // eV -> Joule conversion factor
+constexpr double     J_TO_EV        = 1.0 / E_CHARGE;             // eV <- Joule conversion factor
+
+// collision types
+ constexpr int        COLL_ISO          = 0;                       // isotropic scattering
+ constexpr int        COLL_BACK         = 1;                       // backscattering
+ constexpr int        COLL_ANISO        = 2;                       // anisotropic scattering (screened-Coulomb interaction)
+ constexpr int        COLL_ION          = 3;                       // ionization (e.g. Opal energy share)
+ constexpr int        COLL_DISS         = 4;                       // molecular dissociation
+ constexpr int        COLL_DET          = 5;                       // detachment
+ constexpr int        COLL_NEUT         = 6;                       // neutralization
+ constexpr int        COLL_DISS_ATTACH  = 7;                       // dissociative attachment
+ constexpr int        COLL_DISS_EXC     = 8;                       // dissociative excitation
+ constexpr int        COLL_DISS_REC     = 9;                       // dissociative recombination
+ constexpr int        COLL_ASS_DET      = 10;                      // associative detachment
+ constexpr int        COLL_ISO_FASTATOM = 11;                      // @@@
+ constexpr int        COLL_CHARGE_TRANS = COLL_DISS_ATTACH;        // asymmetric charge transfer 
+
+
+// particle identifiers
+constexpr int        ELE             = 0;                           // electron must be always at first (0) index
+constexpr int        AR_ION          = 1;
+constexpr int        AR_FAST         = 2;     
+constexpr int        AR_META         = 3;     
+constexpr int        AR_GAS          = 4;     
+constexpr int        NE_ION          = 5;     
+constexpr int        NE_FAST         = 6;     
+constexpr int        NE_META         = 7;     
+constexpr int        NE_GAS          = 8;     
+constexpr int        HE_ION          = 9;     
+constexpr int        HE_FAST         = 10;     
+constexpr int        HE_META         = 11;     
+constexpr int        HE_GAS          = 12;     
+constexpr int        O2_P_ION        = 13;
+constexpr int        O2_FAST         = 14;
+constexpr int        O2_META         = 15;
+constexpr int        O2_GAS          = 16;     
+constexpr int        O_M_ION         = 17;
+constexpr int        O_GAS           = 18;
+constexpr int        O3_GAS          = 19;
+
+constexpr int        N_MATERIALS     = 20;
+
+struct material_type{ double charge, mass, Opal_w; string_view name; };
+constexpr material_type MATERIALS [N_MATERIALS] = {        // !!! order must match identifier list above !!!
+    { -E_CHARGE,         E_MASS,  1.0, "electron" },          // ELE        0
+    {  E_CHARGE, 6.63352090e-26, 10.0, "Argon_ion" },         // AR_ION     1
+    {       0.0, 6.63352090e-26, 10.0, "Argon_fast_atom" },   // AR_FAST    2
+    {       0.0, 6.63352090e-26, 10.0, "Argon_metastable" },  // AR_META    3
+    {       0.0, 6.63352090e-26, 10.0, "Argon_gas" },         // AR_GAS     4
+    {  E_CHARGE, 3.35091770e-26, 24.2, "Neon_ion" },          // NE_ION     5
+    {       0.0, 3.35091770e-26, 24.2, "Neon_fast_atom" },    // NE_FAST    6
+    {       0.0, 3.35091770e-26, 24.2, "Neon_metastable" },   // NE_META    7
+    {       0.0, 3.35091770e-26, 24.2, "Neon_gas" },          // NE_GAS     8
+    {  E_CHARGE, 6.64647310e-27, 15.8, "Helium_ion" },        // HE_ION     9
+    {       0.0, 6.64647310e-27, 15.8, "Helium_fast_atom" },  // HE_FAST    10
+    {       0.0, 6.64647310e-27, 15.8, "Helium_metastable" }, // HE_META    11
+    {       0.0, 6.64647310e-27, 15.8, "Helium_gas" },        // HE_GAS     12
+    {  E_CHARGE, 5.31339240e-26, 17.4, "O2p_ion" },           // O2_P_ION   13
+    {       0.0, 5.31339240e-26, 17.4,  "O2_fast_molecule" },  // O2_FAST    14
+    {       0.0, 5.31339240e-26, 17.4,  "O2_metastable" },     // O2_META    15
+    {       0.0, 5.31339240e-26, 17.4,  "O2_gas" },            // O2_GAS     16
+    { -E_CHARGE, 2.65669620e-26, 17.4,  "Om_ion" },            // O_M_ION    17
+    {       0.0, 2.65669620e-26, 17.4,  "O_gas" },             // O_GAS      18
+    {       0.0, 7.97008860e-26, 17.4,  "O3_gas" },            // O3_GAS     19
+};
+
+
+// helper constexpr definition
+template <int Size> constexpr int SUM_ARRAY(const int (&arr)[Size]) {int ret = 0; for (int i=0; i<Size; ++i) ret+=arr[i]; return ret; } 
---
+++ params.h
@@ -0,0 +1,102 @@
+//  params_Ar.h
+//  PICit! parameter file definig discharge conditions for:
+//  Argon gas 
+
+//
+// Simulation Parameters
+//
+constexpr string_view MESSAGE = "PICit! - 1d3v electrostatic PIC/MCC plasma simulation @ Wigner RCP";
+
+// case parameters
+constexpr int         N_G             = 512;                        // number of grid points
+constexpr int         N_T             = 4000;                       // time steps within an RF period
+constexpr double      FREQUENCY       = 10.0e6;                     // driving frequency [Hz]
+constexpr double      VOLTAGE         = 175.0;                      // voltage amplitude [V]
+constexpr double      L               = 0.025;                      // electrode gap [m]
+constexpr double      PRESSURE        = 62.0;                       // gas pressure [Pa]
+constexpr double      TEMPERATURE     = 350.0;                      // background gas temperature [K]
+constexpr double      WEIGHT_COM      = 1e5;                        // common weight of superparticles (see weight factors)
+constexpr double      ELECTRODE_AREA  = 1.0e-4;                     // (fictive) electrode area [m^2]
+constexpr int         N_INIT          = 1000;                      // number of initial electrons and ions
+constexpr int         N_SUB           = 20;                         // ions move only in these cycles (subcycling)
+
+const int             N_EEPF  = 2000;                               // number of energy bins in Electron Energy Probability Function (EEPF)
+const double          DE_EEPF = 0.05;                               // resolution of EEPF [eV]
+const int             N_FED   = 200;                                // number of energy bins in Flux-Energy Distributions (FEDs)
+const double          DE_FED  = 1.0;                                // resolution of FEDs [eV]
+
+constexpr double      B_FIELD         = 0.0;                        // perpendicular magnetic field [T]
+
+// additional (derived) constants
+constexpr double      PERIOD          = 1.0 / FREQUENCY;                           // RF period length [s]
+constexpr double      DT_E            = PERIOD / (double)(N_T);                    // electron time step [s]
+constexpr double      DT_I            = N_SUB * DT_E;                              // ion time step [s]
+constexpr double      DX              = L / (double)(N_G - 1);                     // spatial grid division [m]
+constexpr double      INV_DX          = 1.0 / DX;                                  // inverse of spatial grid size [1/m]
+constexpr double      GAS_DENSITY     = PRESSURE / (K_BOLTZMANN * TEMPERATURE);    // background gas density [1/m^3]
+constexpr double      OMEGA           = TWO_PI * FREQUENCY;                        // angular frequency [rad/s]
+constexpr double      DV              = ELECTRODE_AREA * DX;                       // grod cell volume
+
+// simulation flags
+constexpr bool        SEEDING         = true;                         // additional seeding of particles to ignite plasma
+constexpr bool        WARM_GAS        = false;                        // warm gas approximation for electrons --> if needed, set to true
+constexpr bool        GAS_HEATING     = false;                        // gas heating calculation --> if needed, set to true
+constexpr bool        COULOMB_COLL    = false;                        // Coulomb collisions --> if needed, set to true
+
+// species identifiers
+constexpr int         N_SPECIES                 = 3;                              // number of species in the simulation
+constexpr int         SPECIES_KIND[N_SPECIES]   = { ELE, AR_ION, AR_FAST };
+constexpr double      DT[N_SPECIES]             = { DT_E, DT_I, DT_I};      // time steps
+constexpr double      WEIGHT_FACTORS[N_SPECIES] = { 1.0, 1.0, 15.0};         // superparticle weight factors
+constexpr string_view CS_FILES[N_SPECIES]       = { "ar_e_cs.bin",              // cs files to use (one for each species)
+                                                    "ar_arp_cs.bin",
+                                                    "arf_ar_cs.bin"};     
+
+constexpr int         N_TARGET                  = 1;                               // number of collision target (background) species
+constexpr int         TARGET_KIND[N_TARGET]     = { AR_GAS };                    // materials of collision target (background) species
+constexpr double      TARGET_RATIO[N_TARGET]    = { 1.0 };                             // concentrations of collision target (background) species
+
+// metastable background definition
+constexpr int         META_T_INDEX        = 0;       // index of metastable target, set to 0 for skipping metastable calculation
+constexpr int         META_SOURCE_PROCESS = 6;       // index of metastable source process in CS file assuming species = 0 = electrons
+constexpr int         META_CYCLE_SKIP     = 20;      // No. of RF cycles between metastable density updates
+constexpr double      META_DIFF_0         = 0.015;   // O2 metastable diffusion at 1 Torr, 300 K, [m2/s]
+//constexpr double      META_DIFF_0         = 0.0177;  // Neon metastable diffusion at 1 Torr, 300 K, [m2/s], http://dx.doi.org/10.1088/0022-3727/49/18/185202
+constexpr double      META_REFLECTION     = 0.994;
+
+// XT measurement definition
+constexpr int         SAVE_XT_NUM[N_SPECIES]         = { 0, 0, 0};            // number of processes for XT analysis (max 10 for each species)
+constexpr int         SAVE_XT_PROCESS[N_SPECIES][10] = { {},{} };        // list of processes for XT analysis 
+constexpr int         SAVE_XT_TOT                    = SUM_ARRAY(SAVE_XT_NUM);   // total number of process XT-s to store
+
+// surface coefficients
+constexpr int         N_SIDES              = 2;                     // No. of surfaces: 0 = powered/left, 1 = grounded/right
+constexpr double      SURF_NORMAL[N_SIDES] = {1.0, -1.0};           // surface normal vector directions
+constexpr double      SURF_R_ELE[N_SIDES]  = {0.7, 0.7};            // elastic electron reflection coefficients on two sides
+constexpr double      SURF_E_EMISSION[N_SPECIES][N_SIDES] = { {0.0, 0.0},    // secondary electron emission yields
+                                                              {0.07, 0.07},
+                                                              {0.0, 0.0} }; // TODO        
+
+// Verboncoeour type solution of the Poisson equation
+constexpr double      RESISTANCE   = 0.0;                           // resistor
+constexpr double      INV_C        = 0.0;                           // inverse of capacitance 
+
+// species dependent factors (for electron + heavies system)
+constexpr double  CHARGE(int n)      {return MATERIALS[SPECIES_KIND[n]].charge;}     // charges
+constexpr double  ABSCHARGE(int n)   {return MATERIALS[SPECIES_KIND[n]].charge < 0 ? -MATERIALS[SPECIES_KIND[n]].charge : MATERIALS[SPECIES_KIND[n]].charge;} //abscharge
+constexpr double  MASS(int n)        {return MATERIALS[SPECIES_KIND[n]].mass;}       // masses
+constexpr double  WEIGHT(int n)      {return WEIGHT_COM*WEIGHT_FACTORS[n];}          // superparticle weights 
+constexpr double  FACTOR_D(int n)    {return WEIGHT(n) / DV;}                        // factor linking paricle number per cell to density
+constexpr double  FACTOR_P(int n)    {return DT[n] / MASS(n) * CHARGE(n);}           // factor linking E-field to particle acceleration
+constexpr double  FACTOR_MQ(int n)   {return MASS(n) / ABSCHARGE(n);}                // mass to charge ratios
+
+constexpr double  TARGET_MASS(int t) {return MATERIALS[TARGET_KIND[t]].mass;}                          // target species masses
+constexpr double  OPAL_W(int t)      {return MATERIALS[TARGET_KIND[t]].Opal_w;}                        // Opal w electron spectrum shape parameter
+constexpr double  MU(int n, int t)   {return MASS(n)*TARGET_MASS(t) / ( MASS(n)+TARGET_MASS(t) ); }    // reduced masses
+
+// cross sections
+constexpr int         CS_RANGES            = 1000000;               // number of entries in cross section arrays
+constexpr double      DE_CS                = 0.001;                 // energy division in cross section arrays [eV]
+constexpr int         CS_STRING_LENGTH     = 128;                   // length of comment strings
+
+
